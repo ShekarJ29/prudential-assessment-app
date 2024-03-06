aae90358214152a90cefe5ce83219874cb9a54cd
rajeshpodder64 <159030864+rajeshpodder64@users.noreply.github.com>
2024-03-06 18:51:35 +0530

    Update MenuComponent.js

--- src/components/menu/MenuComponent.js
+++ src/components/menu/MenuComponent.js
@@ -1,17 +1,87 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import './MenuComponent.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Start from '../start/Start';
 
 function MenuComponent() {
   return (
     <div className='navbar-nav-div'>
-      <Navbar bg="dark" data-bs-theme="dark">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#registration">Registration</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-          </Nav>
-      </Navbar>
+       <>
+            <BrowserRouter>
+                <div
+                    style={{
+                        display: "flex",
+                        background: "black",
+                        padding: "5px 0 5px 5px",
+                        fontSize: "16px",
+                    }}
+                >
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "grey"
+                                    : "white",
+                                textDecoration: "none",
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                        >
+                            Home
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/demo"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "grey"
+                                    : "white",
+                                textDecoration: "none",
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                        >
+                            Registration
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/demo"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "grey"
+                                    : "white",
+                                textDecoration: "none",
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                        >
+                            Login
+                        </NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Start />}
+                    />
+                    <Route
+                        exact
+                        path="/demo"
+                        element={<Start />}
+                    />
+                    <Route
+                        exact
+                        path="/demo"
+                        element={<Start />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
       </div>
   );
 }
